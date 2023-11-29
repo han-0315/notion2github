71041a0c831eacd806f43929dbeb224bc48ec756
kane <dongmin7115@gmail.com>
2023-11-29 16:07:24 +0900

    Image download 추가

--- src/component/github.mjs
+++ src/component/github.mjs
@@ -1,10 +1,15 @@
 import { Octokit } from "@octokit/rest";
-
+import fs from 'fs';
+import path from 'path';
 const mapfile_path = ".sync/mapping.json";
-const blog_repo = "han-0315.github.io";
+const repo = "han-0315.github.io";
 const post_base_path = "_posts";
+const image_base_path = "assets/img/post";
 const owner = "han-0315";
+const branch = "main";
 import moment from 'moment-timezone';
+
+// 기본정보
 
 export default class Github {
     octokit;
@@ -25,42 +30,27 @@ export default class Github {
         };
 
         mappingFileContent.push(newContent);
-        const updatedContentString = JSON.stringify(mappingFileContent, null, 2); // 2는 들여쓰기를 위한 것입니다
+        const updatedContentString = JSON.stringify(mappingFileContent, null, 2);
         const base64Content = Buffer.from(updatedContentString).toString('base64');
 
-
-        const { data } = await this.octokit.repos.getContent({
+        await this.pushGitHub({
             owner: owner,
-            repo: blog_repo,
-            path: mapfile_path
-        });
-        const sha = data.sha
+            repo: repo,
+            path: mapfile_path,
+            message: `Update mapping file: ${pageid}`,
+            content: base64Content
+        })
 
 
-        try {
-            const result = await this.octokit.repos.createOrUpdateFileContents({
-                owner: owner,
-                repo: blog_repo,
-                path: mapfile_path,
-                message: `Update mapping file: ${pageid}`,
-                content: base64Content,
-                sha: sha
-            });
-            if (!result || result.status !== 200) {
-                throw new Error("Failed to update the mapping file on GitHub.");
-            }
-        } catch (error) {
-            console.error("Error:", error);
-            throw error;
-        }
     }
-    async post_upload(base64Content, post_title, pageid) {
-        // mapping.json에서 정보 가져오기
+
+
+    async post_upload(filepaths, post_title, pageid) {
         try {
             // GitHub에서 파일 내용 가져오기
             const response = await this.octokit.repos.getContent({
                 owner: owner,
-                repo: blog_repo,
+                repo: repo,
                 path: mapfile_path
             });
             // Base64로 인코딩된 내용을 디코딩
@@ -69,15 +59,15 @@ export default class Github {
 
             // ID가 일치하는 아이템 찾기
             const foundItem = items.find(item => item.id === pageid);
-            console.log(foundItem);
+
 
             if (foundItem == undefined) {
                 console.log("Create Post");
-                await this.post_create(base64Content, post_title, pageid);
+                await this.post_create(filepaths, post_title, pageid);
             }
             else {
                 console.log("Update Post");
-                await this.post_update(base64Content, foundItem.post_title);
+                await this.post_update(filepaths, foundItem.post_title);
             }
 
 
@@ -86,80 +76,108 @@ export default class Github {
         }
     }
 
-    async update(githubRepo, githubPath, base64Content) {
 
-        const { data } = await this.octokit.repos.getContent({
-            owner: owner,
-            repo: githubRepo,
-            path: githubPath
-        });
-        const sha = data.sha;
 
+    async pushGitHub(action) {
         try {
-            const result = await this.octokit.repos.createOrUpdateFileContents({
+            const predContent = await this.octokit.repos.getContent({
                 owner: owner,
-                repo: githubRepo,
-                path: githubPath,
-                message: `Update from Notion: ${githubPath}`,
-                content: base64Content,
-                sha: sha
+                repo: repo,
+                path: action.path
             });
+            if (predContent.status === 200) {
+                // 파일이 존재하면 sha를 action 객체에 추가
+                action.sha = predContent.data.sha;
+            }
+            console.log("File found on GitHub Update File.");
+        } catch (error) {
+            console.log("File not found on GitHub. Creating a new file.");
+        }
+        try {
+            const result = await this.octokit.repos.createOrUpdateFileContents(action);
             if (!result || result.status !== 200) {
                 throw new Error("Failed to update the file on GitHub.");
             }
         } catch (error) {
-            console.error("Error update from GitHub:", error);
-            throw error;
+            console.error("Error while pushing to GitHub:", error);
         }
-
 
     }
 
-
-    async create(githubRepo, githubPath, base64Content) {
-        const result = await this.octokit.repos.createOrUpdateFileContents({
-            owner: owner,
-            repo: githubRepo,
-            path: githubPath,
-            message: `Create from Notion: ${githubPath}`,
-            content: base64Content,
-        });
-        if (!result || result.status !== 201) { // 생성 시 HTTP = 201
-            throw new Error("Failed to create the file on GitHub.");
-        }
-    }
-
-    async upload(base64Content, githubRepo, githubPath) {
-        // 유효성 검사 GitHub PATH
-        try {
-            await this.octokit.repos.getContent({
-                owner: owner,
-                repo: githubRepo,
-                path: githubPath
-            });
-            // GitHub Path가 이미존재
-            console.log("GitHub Path already exists. Update File");
-            this.update(githubRepo, githubPath, base64Content);
-        } catch (error) {
-            if (error.status !== 404) {
-                throw new Error("Error checking the GitHub Path. Please try again.");
-            }
-            console.log("GitHub Path not exists. Create File");
-            this.create(githubRepo, githubPath, base64Content);
-        }
-    }
-    async post_create(base64Content, post_title, page_id) {
+    async post_create(filepaths, post_title, page_id) {
         // 현재 날짜
         const date = moment().tz("Asia/Seoul").format('YYYY-MM-DD');
         const post_full_title = `${date}-${post_title}`;
         const post_path = `${post_base_path}/${post_full_title}.md`;
-        // 블로그 업로드
-        await this.create(blog_repo, post_path, base64Content);
+        const image_path = `${image_base_path}/${post_title}`;
+        const actions = filepaths.map(filepath => {
+            if (filepath.endsWith('.md')) {
+                return {
+                    owner: owner,
+                    repo: repo,
+                    path: post_path,
+                    message: `Create Post: ${post_full_title}`,
+                    content: fs.readFileSync(filepath, 'base64'),
+                };
+            }
+            else {
+                // 이미지 파일
+                return {
+                    owner: owner,
+                    repo: repo,
+                    path: image_path + "/" + path.basename(filepath),
+                    message: `Create Image: ${path.basename(filepath)}`,
+                    content: fs.readFileSync(filepath, 'base64')
+                };
+            }
+
+        });
+
+        for (const action of actions) { // forEach 대신 for...of 사용
+            console.log("Process: " + action.path);
+            await this.pushGitHub(action);
+        }
         // mapping.json에 정보 추가 
         await this.update_mapping(page_id, post_full_title);
     }
-    async post_update(base64Content, post_title) { // 기존에 있는 포스트 이름이 파라미터로 옴
+
+    async post_update(filepaths, post_title) { // 기존에 있는 포스트 이름이 파라미터로 옴
         const post_path = `${post_base_path}/${post_title}.md`;
-        this.update(blog_repo, post_path, base64Content);
+        const post_base = post_title.substring(11);
+        const image_path = `${image_base_path}/${post_base}`;
+
+
+        const actions = filepaths.map(filepath => {
+            if (filepath.endsWith('.md')) {
+                return {
+                    owner: owner,
+                    repo: repo,
+                    path: post_path,
+                    message: `Update Post: ${post_title}`,
+                    content: fs.readFileSync(filepath, 'base64')
+
+                };
+            }
+            else {
+                // 이미지 파일
+                return {
+                    owner: owner,
+                    repo: repo,
+                    path: image_path + "/" + path.basename(filepath),
+                    message: `Update Image: ${path.basename(filepath)}`,
+                    content: fs.readFileSync(filepath, 'base64')
+
+                };
+            }
+
+        });
+
+        for (const action of actions) { // forEach 대신 for...of 사용
+            console.log("Process: " + action.path);
+            await this.pushGitHub(action);
+        }
+
     }
+
+
 }
--- src/handlers/blog.mjs
+++ src/handlers/blog.mjs
@@ -1,15 +1,18 @@
 import { Client } from "@notionhq/client";
 import { NotionToMarkdown } from "notion-to-md";
 import Github from "../component/github.mjs";
-
+import axios from "axios";
+import fs from "fs";
+import os from "os";
 const notion = new Client({
     auth: process.env.NOTION_SECRET_API_KEY,
 });
 // notion to markdown
-
+const image_base_path = "/assets/img/post";
 const n2m = new NotionToMarkdown({ notionClient: notion });
 const github = new Github({});
-
+let imageMapping = [];
+let filePaths = [];
 
 export const Handler = async (event) => {
     if (event.httpMethod !== 'GET') {
@@ -17,7 +20,6 @@ export const Handler = async (event) => {
     }
 
     console.info('received:', event);
-    //console.log(process.env.GITHUB_TOKEN);
 
     const pageId = event.pathParameters.pageid;
     const post_title = decodeURIComponent(event.pathParameters.title);
@@ -34,11 +36,26 @@ export const Handler = async (event) => {
 
     // Notion에서 추출한 데이터 앞에 있는 개행문자 제거 -> layout 형식을 위해
     mdString = mdString.parent.replace(/^\n+/g, '');
+    // Image URL에서 쿼리 파라미터 제거
+    mdString = mdString.replace(/(\.png|\.(jpe?g|gif|bmp|tiff))(\?.*?)?(?=\)|$)/g, "$1");
 
-    // GitHub에 업로드
-    const base64Content = Buffer.from(mdString).toString("base64");
+    const tempdir = os.tmpdir();
+    imageMapping = await downloadImagesFromPage(pageId, tempdir);
+    console.log("Download Image Complete");
 
-    await github.post_upload(base64Content, post_title, pageId);
+    // mdString 문자열 내부 image url을 image name으로 변경
+    imageMapping.forEach(({ imageUrl, ImageN }) => {
+        // 쿼리 파라미터 이전까지의 URL 부분 추출
+        const baseUrl = imageUrl.split('?')[0];
+        const imagePath = image_base_path + "/" + post_title + "/" + ImageN;
+        // 마크다운 문자열 내의 URL을 imagePath 변경
+        filePaths = filePaths.concat([tempdir + "/" + ImageN]);
+        mdString = mdString.replace(new RegExp(baseUrl, 'g'), imagePath);
+    });
+    const mdPath = tempdir + "/" + post_title + ".md";
+    fs.writeFileSync(mdPath, mdString);
+    filePaths = filePaths.concat([mdPath]);
+    await github.post_upload(filePaths, post_title, pageId);
 
     const response = {
         statusCode: 200,
@@ -52,4 +69,45 @@ export const Handler = async (event) => {
     return response;
 }
 
+
+async function downloadImagesFromPage(pageId, basepath) {
+
+    const response = await notion.blocks.children.list({
+        block_id: pageId,
+        page_size: 50,
+    });
+    let id = 1;
+    let localImageMapping = [];
+    for (const block of response.results) {
+        if (block.type === 'image') {
+
+            const imageUrl = block.image.file.url; // 혹은 block.image.external.url;
+            const extensionMatch = imageUrl.match(/\.(png|jpg|jpeg|gif|bmp|tiff)(?=\?|$)/);
+            const ImageN = id + extensionMatch[0];
+            localImageMapping.push({ imageUrl, ImageN });
+            await downloadImage(imageUrl, basepath + "/" + ImageN);
+            id = id + 1;
+        }
+    }
+    return localImageMapping;
+}
+
+
+async function downloadImage(imageUrl, imagePath) {
+    try {
+        const response = await axios({
+            method: 'GET',
+            url: imageUrl,
+            responseType: 'stream'
+        });
+        response.data.pipe(fs.createWriteStream(imagePath));
+
+        return new Promise((resolve, reject) => {
+            response.data.on('end', () => resolve());
+            response.data.on('error', err => reject(err));
+        });
+    } catch (error) {
+        console.error('이미지 다운로드 중 오류 발생:', error);
+    }
+}
 
